a4d7b18e45104383c0a2658ae2d59181243d6169
andika-118140058 <andikas926@gmail.com>
2023-04-14 22:19:00 +0700

    get all books handler

--- src/handler.js
+++ src/handler.js
@@ -4,6 +4,24 @@ const books = require('./books')
 const addBookHandler = (request, h) => {
   const { name, year, author, summary, publisher, pageCount, readPage, reading } = request.payload
 
+  if (!name) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal menambahkan buku. Mohon isi nama buku'
+    })
+    response.code(400)
+    return response
+  }
+
+  if (readPage > pageCount) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount'
+    })
+    response.code(400)
+    return response
+  }
+
   const id = nanoid(16)
   const insertedAt = new Date().toISOString()
   const updatedAt = insertedAt
@@ -18,31 +36,15 @@ const addBookHandler = (request, h) => {
   const isSuccess = books.filter((book) => book.id === id).length > 0
 
   if (isSuccess) {
-    if (!name) {
-      const response = h.response({
-        status: 'fail',
-        message: 'Gagal menambahkan buku. Mohon isi nama buku'
-      })
-      response.code(400)
-      return response
-    } else if (readPage > pageCount) {
-      const response = h.response({
-        status: 'fail',
-        message: 'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount'
-      })
-      response.code(400)
-      return response
-    } else {
-      const response = h.response({
-        status: 'success',
-        message: 'Buku berhasil ditambahkan',
-        data: {
-          bookId: id
-        }
-      })
-      response.code(201)
-      return response
-    }
+    const response = h.response({
+      status: 'success',
+      message: 'Buku berhasil ditambahkan',
+      data: {
+        bookId: id
+      }
+    })
+    response.code(201)
+    return response
   }
 
   const response = h.response({
@@ -53,4 +55,15 @@ const addBookHandler = (request, h) => {
   return response
 }
 
-module.exports = { addBookHandler }
+const getAllBooksHandler = () => ({
+  status: 'success',
+  data: {
+    books: books.map((book) => ({
+      id: book.id,
+      name: book.name,
+      publisher: book.publisher
+    }))
+  }
+})
+
+module.exports = { addBookHandler, getAllBooksHandler }
